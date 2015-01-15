eddccb8744541454f06dc18575c35544bbad5eae
Drew Price <drewprice@users.noreply.github.com>
2015-01-15 13:39:12 -0700

    Created initial controllers: welcome, articles

---
+++ odin-project/misc/blog/app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+	def index
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		render plain: params[:article].inspect
+	end
+
+	def update
+
+	end
+
+	def show
+		
+	end
+
+	def destroy
+		
+	end
+end
