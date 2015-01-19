f2b223d843006e64edd0b3f626159b10a0606163
Drew Price <drewprice@users.noreply.github.com>
2015-01-18 23:31:18 -0700

    Created articles#index, #show, #new

---
+++ odin-project/projects/blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,21 @@
+class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show]
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
+end
