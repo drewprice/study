f9ebcee1d5ddec12b34c19d121b702bc6027c130
Drew Price <drewprice@users.noreply.github.com>
2015-01-20 15:48:06 -0700

    created blog comments, updated css

---
+++ odin-project/projects/blogger/app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+	include CommentsHelper
+
+	def create
+		@comment = Comment.create(comment_params)
+		@comment.article_id = params[:article_id]
+
+		if @comment.save
+			redirect_to article_path(@comment.article)
+		else
+			flash.notice = "Uh oh. Your comment didn't save, try again."
+			redirect_to article_path(@comment.article)
+		end
+	end
+end
--- odin-project/projects/blogger/app/models/comment.rb
+++ odin-project/projects/blogger/app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+
+  validates :author_name, :body,
+  			presence: true
 end
