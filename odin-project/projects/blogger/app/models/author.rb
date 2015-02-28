31d4fdb46b078b91883a39447d153d460b0d5631
Drew Price <drewprice@users.noreply.github.com>
2015-02-28 13:54:09 -0700

    Fully styled

--- odin-project/projects/blogger/app/models/author.rb
+++ odin-project/projects/blogger/app/models/author.rb
@@ -2,6 +2,6 @@ class Author < ActiveRecord::Base
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: "should match confirmation", if: :password
 
-    validates :username, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true, length: { maximum: 11 }
     validates :password, length: { minimum: 4 }
 end
